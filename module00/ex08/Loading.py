eb089ac361da997d7f38abf9d8a71f8158fc1f42
maparigi <maparigi@student.42.fr>
2024-06-14 19:07:16 +0200

    fix ex08

--- module00/ex08/Loading.py
+++ module00/ex08/Loading.py
@@ -28,7 +28,7 @@ def ft_tqdm(lst: range) -> None:
     start = time.time()
 
     full_width = shutil.get_terminal_size().columns - 30
-    tqdm_width = full_width - 20
+    tqdm_width = full_width - 10
 
     for i, val in enumerate(lst, start=1):
         progress = int(i / total * tqdm_width)
@@ -44,4 +44,4 @@ def ft_tqdm(lst: range) -> None:
         progress_info = f"{progress_percentage}%{progress_bar} {i}/{total}"
         time_info = f"[{elapsed_formatted}<{eta_formatted}, {speed:.2f}it/s]"
         print(f"\r{progress_info} {time_info}", end="", flush=True)
-        yield ""
+        yield val
