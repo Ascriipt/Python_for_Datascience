8ebd5255c369f5c6b5802c82c111ee7cddac7180
Red <red@Reds-MacBook-Air.local>
2024-08-17 15:34:54 +0200

    Mod1->ex01 Finished not ready for final push

--- module01/ex01/array2D.py
+++ module01/ex01/array2D.py
@@ -8,8 +8,7 @@ def slice_me(family: list, start: int, end: int) -> list:
 			raise TypeError("One or multiple values are of different sizes.")
 	print(f"My shape is : ({len(family)}, {len(family[0])})");
 	print(f"My new shape is : ({len(family[start:end])}, {len(family[start:end][0])})")
-	print(family[start:end])
-	return [start, end];
+	return family[start:end];
 
 family = [[1.80, 78.4],
 		  [2.15, 102.7],
@@ -17,6 +16,6 @@ family = [[1.80, 78.4],
 		  [1.88, 75.2]]
 
 try: 
-	slice_me(family, 0, 2)
+	print(slice_me(family, 0, 2))
 except Exception as e :
 	print(e)
