ed0c3b4d42155dfa9c3dc8473b0084ccfbb62e50
Red <red@Reds-MacBook-Air.local>
2024-08-17 15:06:26 +0200

    just a few tweaks

--- module01/ex01/array2D.py
+++ module01/ex01/array2D.py
@@ -1,4 +1,14 @@
 def slice_me(family: list, start: int, end: int) -> list:
-	if abs(start) > len(list)
-		
-	return []
+	if len(family) < 1 :
+		raise ValueError;
+	print(f"My shape is : ({len(family)}, {len(family[0])})");
+	print(f"My new shape is : ({len(family[start:end])}, {len(family[start:end][0])})")
+	print(family[start:end])
+	return [start, end];
+
+family = [[1.80, 78.4],
+		  [2.15, 102.7],
+		  [2.10, 98.5],
+		  [1.88, 75.2]]
+
+slice_me(family, 0, 2)
