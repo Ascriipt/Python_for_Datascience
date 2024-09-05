9abf7e236089c72f64ac2fc7694e8390740989be
maparigi lab <maparigi@made-f0cr12s6.paris.europe.42.school>
2024-09-05 12:52:04 +0200

    unfixed embedois shitty fix

--- module00/ex04/whatis.py
+++ module00/ex04/whatis.py
@@ -3,11 +3,12 @@ import sys
 argv = sys.argv[1:]
 
 try:
-    if len(argv) > 1:
-        raise AssertionError("more than one argument is provided")
-    int(argv[0])
-    isEO = "I'm Even." if int(argv[0]) % 2 == 0 else "I'm Odd."
-    print(isEO)
+    for i in argv:
+        if len(argv) > 1:
+            raise AssertionError("more than one argument is provided")
+        int(i)
+        isEO = "I'm Even." if int(i) % 2 == 0 else "I'm Odd."
+        print(isEO)
 
 except AssertionError as e:
     print(f"AssertionError: {e}")
