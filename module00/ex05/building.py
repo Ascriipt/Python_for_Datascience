3b30d98f06aab7eb566b53ebd1771938ec39aa44
maparigi <maparigi@student.42.fr>
2024-09-06 15:42:41 +0200

    fix try except

--- module00/ex05/building.py
+++ module00/ex05/building.py
@@ -5,32 +5,34 @@ def main():
     """
     gets the arguments counts their elements and prints the result
     """
-    argv = sys.argv
-    punctuation = set("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
-    if len(argv) > 2:
-        raise AssertionError()
-    if len(argv) > 1:
-        print(f"The text contains {sum(1 for char in argv[1])} characters:")
-        print(f"{sum(1 for char in argv[1] if char.isupper())} upper letters")
-        print(f"{sum(1 for char in argv[1] if char.islower())} lower letters")
-        print(f"{sum(1 for char in argv[1] if char in punctuation)}", end='')
-        print(" punctuation marks")
-        print(f"{sum(1 for char in argv[1] if char.isspace())} spaces")
-        print(f"{sum(1 for char in argv[1] if char.isdigit())} digits")
-    else:
-        print("What is the text to count?")
-        argument = sys.stdin.readline()
-        print(f"The text contains {sum(1 for char in argument)} characters:")
-        print(f"{sum(1 for char in argument if char.isupper())} upper letters")
-        print(f"{sum(1 for char in argument if char.islower())} lower letters")
-        print(f"{sum(1 for char in argument if char in punctuation)}", end='')
-        print(" punctuation marks")
-        print(f"{sum(1 for char in argument if char.isspace())} spaces")
-        print(f"{sum(1 for char in argument if char.isdigit())} digits")
+    try:
+        argv = sys.argv
+        punctuation = set("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+        if len(argv) > 2:
+            raise AssertionError()
+        if len(argv) > 1:
+            print(f"The text contains {sum(1 for char in argv[1])} characters:")
+            print(f"{sum(1 for char in argv[1] if char.isupper())} upper letters")
+            print(f"{sum(1 for char in argv[1] if char.islower())} lower letters")
+            print(f"{sum(1 for char in argv[1] if char in punctuation)}", end='')
+            print(" punctuation marks")
+            print(f"{sum(1 for char in argv[1] if char.isspace())} spaces")
+            print(f"{sum(1 for char in argv[1] if char.isdigit())} digits")
+        else:
+            print("What is the text to count?")
+            argument = sys.stdin.readline()
+            print(f"The text contains {sum(1 for char in argument)} characters:")
+            print(f"{sum(1 for char in argument if char.isupper())} upper letters")
+            print(f"{sum(1 for char in argument if char.islower())} lower letters")
+            print(f"{sum(1 for char in argument if char in punctuation)}", end='')
+            print(" punctuation marks")
+            print(f"{sum(1 for char in argument if char.isspace())} spaces")
+            print(f"{sum(1 for char in argument if char.isdigit())} digits")
+    except AssertionError:
+        print("AssertionError : Y'a une erreur connard.")
+    except Exception:
+        print("AssertionError : Y'a une erreur connard.")
 
 
 if __name__ == "__main__":
-    try:
-        main()
-    except Exception:
-        print("AssertionError : Y'a une erreur connard.")
+    main()
--- module00/ex06/filterstring.py
+++ module00/ex06/filterstring.py
@@ -1,7 +1,6 @@
 import sys
 import ft_filter
 
-
 def filterstring(tofilter: str, n: int):
     """_summary_
 
@@ -9,8 +8,7 @@ def filterstring(tofilter: str, n: int):
         tofilter (str): the string to parse
         n (int): the length to filter
     """
-    parsed_list = [x for x in tofilter.split()]
-    print(ft_filter.ft_filter(lambda i: len(i) > n, parsed_list))
+
 
 
 def main():
@@ -19,17 +17,19 @@ def main():
     Raises:
         AssertionError: in case there aren't enough parameters
     """
-    argv = sys.argv
-    if len(argv) < 3 or len(argv) > 3:
-        raise AssertionError("The arguments are bad")
-    else:
-        filterstring(argv[1], int(argv[2]))
-
-
-if __name__ == "__main__":
     try:
-        main()
+        argv = sys.argv
+        if len(argv) < 3 or len(argv) > 3:
+            raise AssertionError("The arguments are bad")
+        tofilter = argv[1]
+        n = int(argv[2])
+        parsed_list = [x for x in tofilter.split()]
+        print(ft_filter.ft_filter(lambda i: len(i) > n, parsed_list))
     except AssertionError as e:
         print(f'AssertionError: {e}')
     except ValueError as e:
         print(f"ValueError: {e}")
+
+
+if __name__ == "__main__":
+    main()
